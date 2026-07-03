eb97579540939d24f842d23dd40a3dee71ecb39a
Jerson Calderón <asijerson@gmail.com>
2026-07-03 09:30:00 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/com/espe/tech/SpringWebfluxReservationEventsApplication.java
@@ -0,0 +1,12 @@
+package com.espe.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReservationEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReservationEventsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/espe/tech/SpringWebfluxReservationEventsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.espe.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxReservationEventsApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
